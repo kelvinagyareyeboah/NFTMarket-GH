2cf720a2a4c77da3853e85d9b3b0a0448f2b12a3
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-10 02:00:02 +0000

    Update EIP712Base.sol

--- opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/EIP712Base.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solid
-import {Initializable} from "./Initializable.sol";
-
+import {Initializable} from "./Initi
 /**
  * @title EIP712Base (Improved)
  * @dev Optimized and secure EIP-712 implementation
