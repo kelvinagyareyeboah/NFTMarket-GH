3672217f0269a8cced24328cbeb113b912a53a3f
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-11 01:07:17 +0000

    Update TestForReentrancyAttack.sol

--- opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
+++ opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
@@ -1,5 +1,4 @@
-
-import "@openzeppelin/contracts/access/Ownable.sol";
+/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
