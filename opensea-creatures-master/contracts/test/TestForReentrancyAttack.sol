e5de98ccb08fe89a83fab53556bc993041f92ac2
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-11 01:15:25 +0000

    Update TestForReentrancyAttack.sol

--- opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
+++ opensea-creatures-master/contracts/test/TestForReentrancyAttack.sol
@@ -1,6 +1,4 @@
-55Receiver, Ownable {
-    // ERC-1155 receiver return signatures
-    bytes4 private constant ERC1155_RECEIVED_SIG = 0xf23a6e61;
+55Receiver, Ownable {RECEIVED_SIG = 0xf23a6e61;
     bytes4 private constant ERC1155_BATCH_RECEIVED_SIG = 0xbc197c81;
     bytes4 private constant INTERFACE_ERC1155_RECEIVER_FULL = 0x4e2312e0;
     bytes4 private constant INTERFACE_ERC165 = 0x01ffc9a7;
