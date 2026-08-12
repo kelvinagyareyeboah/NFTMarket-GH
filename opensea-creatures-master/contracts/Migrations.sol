2fcff7fa7e32c3129e9be9d74bc23d93135a2b18
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-12 01:14:08 +0000

    Update Migrations.sol

--- opensea-creatures-master/contracts/Migrations.sol
+++ opensea-creatures-master/contracts/Migrations.sol
@@ -3,8 +3,7 @@
     /**
      * @dev Sets the last completed migration number
      * @param comigration number to record as
-    function setCompleted(uint completed) public rest
-    /**
+    function setCompleted(uint completed) 
      * @dev Allows upgrad contract
      *      and sets the last completed migration in the new contract
      * @param new_address The address of the new Mig
