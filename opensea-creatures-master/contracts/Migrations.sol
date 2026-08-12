ee1739d2699939b9ea767adb94bc2ce731adf8b9
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-12 01:14:38 +0000

    Update Migrations.sol

--- opensea-creatures-master/contracts/Migrations.sol
+++ opensea-creatures-master/contracts/Migrations.sol
@@ -8,7 +8,7 @@
      *      and sets the last comn in the nntract
      * @param new_address The addr
     function s new_address) public restricted {
-        Migrations upgraded = Migrations(new_address);
+        Migrations upgrations(new_address);
         upgraded.setCompleted(last_completed_m
     }
 }
