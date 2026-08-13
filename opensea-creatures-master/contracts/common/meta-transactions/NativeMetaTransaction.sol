e1ba388e33bbf7d7b7c13f1f4763e8dc20f03dc2
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-13 23:43:00 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -1,7 +1,7 @@
 
         address recovered = _recoverSigner(digest, signature);
         require(recovered == user, "MetaTx: invalid signature");
-        // increment nonce BEFORE
+        // increment n
         _nonces[user]++;
 
         emit MetaTransactionExecuted(user, msg.sender, functionSignature);
