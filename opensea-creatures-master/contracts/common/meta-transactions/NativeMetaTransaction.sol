26100f7c3c779f79fddd5c8bcb5aa701f88cc408
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-13 23:16:32 +0000

    Update NativeMetaTransaction.sol

--- opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/NativeMetaTransaction.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import {EIP712Base} from "./EIP712Base.sol";
+pragma solidity ^0.8.20;m "./EIP712Base.sol";
 
 /**
  * @title NativeMetaTransaction (Improved)
