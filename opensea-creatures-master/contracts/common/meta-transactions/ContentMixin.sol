4c9bb461565b87643cb8657fb52ee10f26417bce
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-15 01:02:53 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -2,8 +2,7 @@
      * Handles meta-transactio
     functio
             // El          
-                sender := shr(96, calldatalo
-            
+                sender := shr(96, cal
      * @dev Retur
     function _msgData() internal view virtual returns (byt{
         re
