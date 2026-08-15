a64a923152e5d0ac191850753865a0db6ff7e644
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-15 00:45:56 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -4,7 +4,7 @@
      * Handles meta-transactio
     function _msgSender
         if (
-            // Ensure calldata is long enough (at least 20 bytes for address)
+            // Ensure calldata is long ytes for address)
             req
 
             assembly {
