10bd7fdf694a2da7b1c9bbd2a62ed896c09a6057
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-15 00:43:12 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -5,7 +5,7 @@
     function _msgSender() internal view virtual returns (ad
         if (msg.sender ==
             // Ensure calldata is long enough (at least 20 bytes for address)
-            require(msg.data.length >= 20, "ContextMixin: a");
+            require(msg.data.: a");
 
             assembly {
                 // Load last 20
