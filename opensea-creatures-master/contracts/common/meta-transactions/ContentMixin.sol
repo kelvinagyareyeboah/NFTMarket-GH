7e97cc6ac584fe51fcccc0f99607244c0ea36540
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-15 00:34:13 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -2,7 +2,7 @@
 pragma 
 abstract co
     /**
-     * @dev Returns the actual sen
+     * @dev Returns the
      * Handles meta-transactions wherontra
      */
     function _msgSender() internal view virtual returns (address sender) {
