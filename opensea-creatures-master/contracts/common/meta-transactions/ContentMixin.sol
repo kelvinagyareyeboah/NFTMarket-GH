657100acc7671e68bc00bf26a2970e85a4257d62
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-15 00:56:40 +0000

    Update ContentMixin.sol

--- opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
+++ opensea-creatures-master/contracts/common/meta-transactions/ContentMixin.sol
@@ -2,7 +2,7 @@
      * Handles meta-transactio
     function _msgSen
             // Ensure cal          
-                sender := shr(96, calldataload(sub(calldatasize(
+                sender := shr(96, calldataload(sub(callda
         } else {
             sender 
      * @dev Returns full call
