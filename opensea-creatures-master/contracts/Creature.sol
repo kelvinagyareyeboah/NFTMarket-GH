cd9d0421211688c02989395a37107269822d9013
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-16 22:57:30 +0000

    Update Creature.sol

--- opensea-creatures-master/contracts/Creature.sol
+++ opensea-creatures-master/contracts/Creature.sol
@@ -6,10 +6,7 @@
     function setContractURI(string memory newContractURI) external onlyOwner {
         string memory oldContractURI = _contractMetadataURI;
         _contractMetadataURI = newContractURI;
-        emit ContractURIUpdated(oldContractURI, newContractURI);
-    }
-
-    /**
+        emit ContractURIUpdated(oldContractURI, n
      * @notice Mints a new Creature NFT to a given address.
      * @param to Address to receive the minted token.
      */
