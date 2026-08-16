86bb5a99972a820d5fb6069d0972b6e3de68375f
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-16 22:54:00 +0000

    Update Creature.sol

--- opensea-creatures-master/contracts/Creature.sol
+++ opensea-creatures-master/contracts/Creature.sol
@@ -1,4 +1,4 @@
-tps://creatures-api.opensea.io/api/creature/";
+ensea.io/api/creature/";
         _contractMetadataURI = "https://creatures-api.opensea.io/contract-metadata.json";
     // Public & External Functions
     // --------------------------
