d89d0a5daea293b593e515a78bfcca3d5aefd66b
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-16 22:52:04 +0000

    Update Creature.sol

--- opensea-creatures-master/contracts/Creature.sol
+++ opensea-creatures-master/contracts/Creature.sol
@@ -1,5 +1,4 @@
 
- * @title Creature
  * @dev A tradable ERC721 NFT contract for unique creatures, compatible with OpenSea.
  *      Inherits from ERC721Tradable for marketplace-friendly minting and p
 contract Creature is ERC721Tradable {
