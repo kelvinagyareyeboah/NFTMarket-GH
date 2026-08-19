2bd91b633981efa00fd788b2131bef6d4314f31d
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-19 22:54:33 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -1,5 +1,4 @@
-sProbabilities Arra to select each item cla     * Callable only by t
-        uint256 _
+
         LootBoxRandomness.setOptionSettings(state, _option, _maxQuantityPerOpen, _classProbabilities, _guarantees);
     }
 
