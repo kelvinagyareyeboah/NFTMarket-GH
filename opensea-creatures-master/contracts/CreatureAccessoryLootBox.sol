036356fb14f14e02ac9d2cd2c94ef34c00eb03bd
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-19 22:12:36 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -5,7 +5,7 @@
      * @param _option ID of the lootbox option (e.g., bronze, silver, gold).
      * @param _maxQuantityPerOpen Maximum number of NFTs .
      * @param _classProbabilities Arra to select each item class.
-     * @param _guarantee guaper open.
+     * @param _guarantee 
      * Callable only by t
         uint256 _maxQuantityPerOpen,
         uint16[] memory _classProbabilities,
