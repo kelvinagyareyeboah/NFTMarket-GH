7d48592b5aebea937288fe81300af95a6901bc59
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-19 22:53:16 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -1,5 +1,4 @@
-
-     * @param  ID of the lootbox option (e.g., bronze, silver, gold).
+ the lootbox option (e.g., bronze, silver, gold).
      * @param _maxQuantityPerOpen Maximum n
      * @param _classProbabilities Arra to select each item cla     * Callable only by t
         uint256 _ma
