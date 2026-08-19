e1e36e615c0c14ec1a1a08223715066f0e79323c
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-19 22:55:24 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -4,7 +4,7 @@
     // MAIN U
      * @notice Oded NFTs to the recipis Address that will rece
      * @param _amount Number of lo
-     * Anyone holding lootboxes can call this.
+     * Anyone holding lootboxes
      */
     function unpack(
         uint256 _optionId,
