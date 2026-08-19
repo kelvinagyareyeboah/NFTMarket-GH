b74f79ec143388f5973d37254a01217d85b7c7d0
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-19 22:54:57 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -1,9 +1,7 @@
 
         LootBoxRandomness.setOptionSettings(state, _option, _maxQuantityPerOpen, _classProbabilities, _gu
     ////////////////////////////////////
-    // MAIN USER INTERACTION
-
-    /**
+    // MAIN U
      * @notice Opens a lootbox, nded NFTs to the recipie     * @param _toAddress Address that will rece
      * @param _amount Number of lootboxes to open.
      * Anyone holding lootboxes can call this.
