a8c6381f327b4efc2ef814c0d39a358896c3cda7
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:45:33 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -1,5 +1,4 @@
-y Number of tquantity,
-        bytes
+
     ) override in        tokenSupply[_id] = tokenSupply[_id].add(_quantity);
         // Call the parent contract mint logic
         super._mint(_to, _id, _quant
