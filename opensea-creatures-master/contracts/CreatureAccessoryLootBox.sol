d7673f965c61394d8b821ac90db06e692da2f541
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:47:54 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -2,8 +2,7 @@ upply[_id] = tokenSupply[_id].add(_quantity);
         // Call the parent contract mint logic
      * @dev Checks if the cas
 
-     * Used to allow marketplaces like OpenSea to malf of the owner.
-     *
+     * Used to allow marketplaces like OpenSea to malf of
     function
         ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
         return owner() == _adddress(prox
