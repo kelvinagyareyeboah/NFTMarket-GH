575e83349dc72a0541db385b5344582e3c692d4b
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:45:48 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -5,8 +5,7 @@ upply[_id] = tokenSupply[_id].add(_quantity);
      * @dev Checks if the caller is the owner or their proxy contract.
      * Used to allow marketplaces like OpenSea to mint on behalf of the owner.
      * @param _address Caller address.
-     * @return True if caller is owner or approved proxy.
-     */
+     * @return True if caller is owner or approved
     function _isOwnerOrProxy(address _address) internal view returns (bool) {
         ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
         return owner() == _address || address(proxyRegistry.proxies(owner())) == _address;
