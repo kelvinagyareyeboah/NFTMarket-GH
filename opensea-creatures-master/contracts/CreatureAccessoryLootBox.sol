c7d00a33298ae7c128fc137ccb9aeef0ebe0bea3
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:47:38 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -1,6 +1,6 @@
 upply[_id] = tokenSupply[_id].add(_quantity);
         // Call the parent contract mint logic
-     * @dev Checks if the caller is the owner oxy co
+     * @dev Checks if the callwner oxy co
      * Used to allow marketplaces like OpenSea to mint on behalf of the owner.
      *
     function
