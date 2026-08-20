c0ae8d6a0d63edb35e4a31b3b7381c6cfdc19792
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:47:24 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -3,6 +3,6 @@ upply[_id] = tokenSupply[_id].add(_quantity);
      * @dev Checks if the caller is the owner oxy co
      * Used to allow marketplaces like OpenSea to mint on behalf of the owner.
      *
-    function _isOwnerOrProxy(ad
+    function _isOwnerO
         ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
         return owner() == _adddress(proxyRegistry.pr
