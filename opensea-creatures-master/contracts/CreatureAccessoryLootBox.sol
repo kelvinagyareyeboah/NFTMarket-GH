0cfe3e6ddce21266ce185524775bed23e757fa20
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:46:38 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -2,7 +2,7 @@ upply[_id] = tokenSupply[_id].add(_quantity);
         // Call the parent contract mint logic
      * @dev Checks if the caller is the owner or their proxy contract.
      * Used to allow marketplaces like OpenSea to mint on behalf of the owner.
-     * @param _address Caller address.
+     * @param _address Calle
      * @return True if caller
     function _isOwnerOrProxy(ad
         ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
