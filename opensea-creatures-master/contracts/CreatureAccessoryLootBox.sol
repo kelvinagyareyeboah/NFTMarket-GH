bb3f64a73f5b5df457985154a696d4a645961758
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 16:47:11 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -2,8 +2,7 @@ upply[_id] = tokenSupply[_id].add(_quantity);
         // Call the parent contract mint logic
      * @dev Checks if the caller is the owner oxy contract.
      * Used to allow marketplaces like OpenSea to mint on behalf of the owner.
-     * @para
-     * @r
+     *
     function _isOwnerOrProxy(ad
         ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
         return owner() == _adddress(proxyRegistry.pr
