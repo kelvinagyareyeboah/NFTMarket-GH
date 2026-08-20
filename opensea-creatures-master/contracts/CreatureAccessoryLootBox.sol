815d5e1e3804bc2e2876971a95b70f053a5c6c57
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-20 14:24:00 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -2,7 +2,7 @@
      * @dev Internal mint function overrides base mint.
      * @param _to Recipient address.
      * @param _id Token ID.
-     * @param _quantity Number of tok    function _mint(
+     * @param _quantity Number of tok  
         address _to,
         uint256 _id,
         uint256 _quantity,
