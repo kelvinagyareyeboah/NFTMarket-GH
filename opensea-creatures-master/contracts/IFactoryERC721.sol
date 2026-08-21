d39fbbf510d69550a426cca71e6de0f84e9e6426
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-21 01:26:14 +0000

    Update IFactoryERC721.sol

--- opensea-creatures-master/contracts/IFactoryERC721.sol
+++ opensea-creatures-master/contracts/IFactoryERC721.sol
@@ -1,7 +1,1 @@
-s
 
-     *  1. User interacts with marketplace and selects "Mint Rare NFT".
-     *  2. Marketplace calls `mint(1, userAddress)` on this factory.
-     *  3. Factory creates and transfers the new NFT(s) to `userAddress`.
-     * ```
-     *
