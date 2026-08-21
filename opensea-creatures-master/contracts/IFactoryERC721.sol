34a06bda9c1ee5869745f7744123cf24bd37746c
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-21 01:26:08 +0000

    Update IFactoryERC721.sol

--- opensea-creatures-master/contracts/IFactoryERC721.sol
+++ opensea-creatures-master/contracts/IFactoryERC721.sol
@@ -5,9 +5,3 @@ s
      *  3. Factory creates and transfers the new NFT(s) to `userAddress`.
      * ```
      *
-     * @param _optionId The numeric identifier for the chosen minting option.
-     * @param _toAddress The recipient address that will own the minted NFT(s).
-     */
-    function mint(uint256 _optionId, address _toAddress) external;
-}
-
