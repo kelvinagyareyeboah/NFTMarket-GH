e76c5b4c0ea28139b727050460459bfa40ea2aef
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-21 00:51:20 +0000

    Update CreatureAccessoryLootBox.sol

--- opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
+++ opensea-creatures-master/contracts/CreatureAccessoryLootBox.sol
@@ -2,5 +2,4 @@ upply[_id] = tokenSupply[_id].add(_quantity);
         // Call the parent contract
      * @dev Checks if th
      * Used to allow marketpl
-        ProxyRegistry proxyRegistry = ProxyRe(proxyRegistryAddress);
-        r
+        ProxyRegistry proxyRegistry = ProxyRe(proxyRegistryAdd
